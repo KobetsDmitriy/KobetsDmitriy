99530da230f449aac17e5e2bf6c5a38a02e3d6b5
Kobets Dmitriy <kobetsdmitriya@gmail.com>
2022-04-11 17:10:00 +0300

    add day 5

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,48 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        Car car = new Car();
+        car.setModel("Audi");
+        car.setColor("Red");
+        car.setAge(2022);
+        System.out.println(car.getModel());
+        System.out.println(car.getColor());
+        System.out.println(car.getAge());
     }
 }
+class Car{
+    private String model;
+    private String color;
+    private int age;
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+/*
+1. Создать класс Автомобиль (англ. Car) с полями “Модель”, “Цвет”, “Год выпуска”. Создать get и set методы
+для каждого поля. Создать экземпляр класса Автомобиль. Задать значение для каждого поля, используя set методы.
+Вывести в консоль значение каждого из полей, используя get методы.
+Созданный вами класс должен отвечать принципам инкапсуляции.
+
+ */
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,36 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        Motorbike motorbike = new Motorbike("Yamaha", "Red", 2022);
+        System.out.println(motorbike.getModel());
+        System.out.println(motorbike.getColor());
+        System.out.println(motorbike.getAge());
     }
 }
+class Motorbike{
+    private String model;
+    private String color;
+    private int age;
+
+    public Motorbike(String model, String color, int age) {
+        this.model = model;
+        this.color = color;
+        this.age = age;
+    }
+    public String getModel(){
+        return model;
+    }
+    public String getColor(){
+        return color;
+    }
+    public int getAge(){
+        return age;
+    }
+}
+
+/*
+2. Создать класс Мотоцикл (англ. Motorbike) с полями “Модель”, “Цвет”, “Год выпуска”. Создать экземпляр
+класса Мотоцикл, используя конструктор (set методы не использовать). Необходимо придерживаться принципов
+инкапсуляции и использовать ключевое слово this. Вывести в консоль значение каждого из полей, используя get методы.
+
+*/
